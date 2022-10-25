8f8ceaff71fe6a5df5397e3b4be9af1856f3b6a8
Glitchy Crafting <glitchycrafting@gmail.com>
2022-10-24 19:24:31 -0500

    Made CSS more responsive

--- build.sh
+++ build.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-cmake .
+mkdir build
+cd ./build
+
+cmake ../src
 if [[ $? -gt 0 ]]; then
   echo "Fail-ed (Cmake)"
+  cd ..
   exit 1
 fi
 echo "Cmake-d"
@@ -10,14 +14,15 @@ echo "Cmake-d"
 make
 if [[ $? -gt 0 ]]; then
   echo "Fail-ed (Make)"
+  cd ..
   exit 2
 fi
 echo "Make-d"
 
-cd ./build
 ./WDHANY
 if [[ $? -gt 0 ]]; then
   echo "Fail-ed"
+  cd ..
   exit 3
 fi
 echo "Ran-ed"
