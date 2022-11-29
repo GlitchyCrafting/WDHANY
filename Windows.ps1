30f2f4c74d7aeb9f4d2f58a743b9d03eb0d1b0cc
Glitchy Crafting <glitchycrafting@gmail.com>
2022-11-29 17:38:37 -0600

    Delete Windows.ps1

--- Windows.ps1
+++
@@ -1,8 +0,0 @@
-cargo build --release --target=x86_64-pc-windows-msvc
-mkdir WDHANY
-
-cp ./target/release/wdhany ./WDHANY
-cp ./resources/db.sqlite ./WDHANY
-cp ./resources/sqlite.dll ./WDHANY
-cp ./resources/Rocket.toml ./WDHANY
-
